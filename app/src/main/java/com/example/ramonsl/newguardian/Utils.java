e8a1ebaf88f65b814a6a667507284d9086afba1c
ramonsl <ramonsl@gmail..com>
2018-07-04 19:30:54 -0300

    Alterações

--- app/src/main/java/com/example/ramonsl/newguardian/Utils.java
+++ app/src/main/java/com/example/ramonsl/newguardian/Utils.java
@@ -2,33 +2,29 @@ package com.example.ramonsl.newguardian;
 
 import android.text.TextUtils;
 import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 
-
 public class Utils {
     private static final String LOG_TAG = Utils.class.getSimpleName();
+
     private Utils() {
 
     }
+
     public static ArrayList<News> extractNewsFromJson(String newsJSON) {
         if (TextUtils.isEmpty(newsJSON)) {
             return null;
         }
-       ArrayList<News> newses = new ArrayList<>();
+        ArrayList<News> newses = new ArrayList<>();
         try {
 
             JSONObject baseJsonResponse = new JSONObject(newsJSON);
@@ -77,29 +73,4 @@ public class Utils {
         }
         return newses;
     }
-
-    private static URL gerarUrl(String stringUrl) {
-        URL url = null;
-        try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "URL INVALIDA ", e);
-        }
-        return url;
-    }
-
-    private static String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-        return output.toString();
-    }
-
 }
